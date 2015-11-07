0ea5dcfc0a018d18d7abb86f4f799cebb40ebf46
Fabio Gottlicher <fabiog@ifit.com>
2015-11-07 12:45:37 -0700

    ad

--- app.js
+++ app.js
@@ -260,7 +260,7 @@ app.post('/api/vote/dislike/:userId',jsonParser, function(req, res){
 
 app.get('/api/matches/:userId', function(req, res){
 	userCollection.find({} , function (err, doc){
-		if(err) thrown err;
+		if(err) throw err;
 		var scores = {}
 		var currUser = _.find(doc, function(obj) { return obj.name == req.params.userId })
 		console.log("currUser")
