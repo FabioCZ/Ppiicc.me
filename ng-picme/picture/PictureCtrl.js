e37068296d69ff940f4236f5892162ed4dd5d666
Ryan Johnson <ryanzj725@hotmail.com>
2015-11-06 20:39:04 -0500

    PictureCtrl PictureService

---
+++ ng-picme/picture/PictureCtrl.js
@@ -0,0 +1,14 @@
+picmeApp.controller('PictureCtrl', ['pictureService', '$scope', pictureService, function($scope) {
+	// Controller Scope
+	var PictureCtrl = this;
+	PictureCtrl.scope = $scope;
+	
+	// Run the controller
+	PictureCtrl.Init();
+	
+	/* Method Definitions */
+	
+	PictureCtrl.Init = function() {
+		
+	};
+}]);
