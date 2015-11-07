cb1e012daed054d69ba58017ff1f4d97b64c5441
Fabio Gottlicher <fabiog@ifit.com>
2015-11-07 11:03:41 -0700

    ad

--- app.js
+++ app.js
@@ -181,6 +181,7 @@ app.post('/api/user/userId', jsonParser, function(req, res){
 })
 
 app.post('api/vote/like',jsonParser, function(req, res){
+	console.log("voting")
 	userCollection.findOne({'name':  req.params.userId},function(err, doc){
 		if(doc){
 			doc.liked.push(req.body)
