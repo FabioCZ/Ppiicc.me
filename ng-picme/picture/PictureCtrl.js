3f78967b0a5d96e5c33fc2a9cbf461dc5cea68c7
Greg Vernon <me@gregvernon.com>
2015-11-06 19:23:05 -0700

    Picture

--- ng-picme/picture/PictureCtrl.js
+++ ng-picme/picture/PictureCtrl.js
@@ -3,12 +3,22 @@ picmeApp.controller('PictureCtrl', ['pictureService', '$scope', pictureService, 
 	var PictureCtrl = this;
 	PictureCtrl.scope = $scope;
 	
-	PictureCtrl.picture = [];
+	// A picturrrr up in hurrrrr
+	PictureCtrl.Picturrrrr = [];
 	
 	// Run the controller
 	PictureCtrl.Init();
 	
 	/* Method Definitions */
+	
+	PictureCtrl.DislikePicture = function() {
+		PictureCtrl.Picturrrrr.vote = false;
+		pictureService.submitVote(PictureCtrl.Picturrrr)
+			.then(function(data)
+			{
+				// Maybe display a message here or something
+			});
+	}
 	
 	PictureCtrl.Init = function() {
 		pictureService.getPicture().then(function(data) {
