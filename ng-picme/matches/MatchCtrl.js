952bc383a6d7e5ba36bc2d4a2294acd78ec05f16
Greg Vernon <me@gregvernon.com>
2015-11-06 20:05:54 -0700

    Match Picture

--- ng-picme/matches/MatchCtrl.js
+++ ng-picme/matches/MatchCtrl.js
@@ -1,4 +1,18 @@
-picmeApp.controller('MatchCtrl', function()
+picmeApp.controller('MatchCtrl', ['matchService', '$scope', function(matchService, $scope)
 {
+	var MatchCtrl = this;
+	MatchCtrl.scope = $scope;
 	
-})
+	MatchCtrl.Matches = [];
+	
+	MatchCtrl.Init();
+	
+	/* Method Declarations/Definitions */
+	
+	MatchCtrl.Init = function(matchService)
+	{
+		matchService.getMatches().then(function (data) {
+			MatchCtrl.Matches = data;
+		});
+	}
+}]);
